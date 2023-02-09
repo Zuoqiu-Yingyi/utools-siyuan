5244c966561537e281c6f630c6ed11d9b95253a9
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-02-09 23:54:33 +0800

    build:  配置 vite 生产环境打包选项 | Config vite production build options.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 - 初始化项目 | Init project
 - 添加项目依赖 `utools-api-types` 与 `utools-helper` | Add dependencies `utools-api-types` and `utools-helper`.
 - 创建功能 `config` | Create a feature `config`.
+- 配置 vite 生产环境打包选项 | Config vite production build options.
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                preload: './src/preload.ts',
+            },
+            output: {
+                // entryFileNames: '[name].js',
+                entryFileNames: chunkInfo => {
+                    switch (chunkInfo.name) {
+                        case 'preload':
+                            return '[name].js';
+
+                        default:
+                            return 'assets/[name]-[hash].js';
+                    }
+                },
+            }
+        },
+    },
 })
